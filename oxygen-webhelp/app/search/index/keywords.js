2996c12e2565921e5175e0e612c269ddab6ade0b
madzka <maosak@gmail.com>
2021-05-11 18:30:23 +0200

    files added SolarSystem

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,16 @@
+define(function() {var keywords=[{w:"My",p:["p0"]},{w:"first",p:["p0"]},{w:"telescope",p:["p0"]},{w:"SkyWatcher",p:["p0"]},{w:"Observing",p:["p1"]},{w:"planets",p:["p1","p8"]},{w:"Rotating",p:["p2"]},{w:"sky",p:["p2","p7"]},{w:"map",p:["p2"]},{w:"Links",p:["p3"]},{w:"Useful",p:["p4","p6"]},{w:"applications",p:["p4"]},{w:"YouTube",p:["p5"]},{w:"channels",p:["p5"]},{w:"AstroAccessories",p:["p6"]},{w:"Explore",p:["p7"]},{w:"the",p:["p7","p8"]},{w:"night",p:["p7"]},{w:"When,",p:["p8"]},{w:"where",p:["p8"]},{w:"and",p:["p8"]},{w:"how",p:["p8"]},{w:"to",p:["p8"]},{w:"see",p:["p8"]},{w:"in",p:["p8"]},{w:"2021",p:["p8"]}];
+var ph={};
+ph["p0"]=[0, 1, 2, 3];
+ph["p1"]=[4, 5];
+ph["p2"]=[6, 7, 8];
+ph["p3"]=[9];
+ph["p4"]=[10, 11];
+ph["p5"]=[12, 13];
+ph["p6"]=[10, 14];
+ph["p7"]=[15, 16, 17, 7];
+ph["p8"]=[18, 19, 20, 21, 22, 23, 16, 5, 24, 25];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
